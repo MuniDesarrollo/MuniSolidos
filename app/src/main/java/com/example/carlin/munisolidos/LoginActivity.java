ed6cd214b69251a2574934bf332531ef9e4b3d28
INFO_LAPTOP <acostasimpl@gmail.com>
2018-02-05 09:52:36 -0500

    reporte

--- app/src/main/java/com/example/carlin/munisolidos/LoginActivity.java
+++ app/src/main/java/com/example/carlin/munisolidos/LoginActivity.java
@@ -4,15 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.carlin.munisolidos.view.CreateCiudadanoActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnLogin;
+    EditText txtUsuario,txtCOntraseña;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtUsuario=(EditText)findViewById(R.id.txtUsuario);
+        txtCOntraseña=(EditText)findViewById(R.id.txtContraseña);
+        
+        btnLogin=(Button)findViewById(R.id.btnLogin);
+
     }
     public void goCreateCiudadano(View view)
     {
