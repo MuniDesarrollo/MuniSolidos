8581a03a11b8a06162ee2874b8c0021da7e8b3e2
INFO_LAPTOP <acostasimpl@gmail.com>
2018-02-05 10:36:42 -0500

    reporte ok

--- app/src/main/java/com/example/carlin/munisolidos/LoginActivity.java
+++ app/src/main/java/com/example/carlin/munisolidos/LoginActivity.java
@@ -8,6 +8,16 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.carlin.munisolidos.view.CreateCiudadanoActivity;
+import com.example.carlin.munisolidos.view.ReporteSolidosActivity;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,13 +31,55 @@ public class LoginActivity extends AppCompatActivity {
 
         txtUsuario=(EditText)findViewById(R.id.txtUsuario);
         txtCOntraseña=(EditText)findViewById(R.id.txtContraseña);
-        
+
         btnLogin=(Button)findViewById(R.id.btnLogin);
 
     }
+    //ruta de la vista reporte
+
+    public void goreporteCiudadano(View view)
+    {
+        Intent intn=new Intent(this, ReporteSolidosActivity.class);
+        startActivity(intn);
+    }
+
+    //funcion que muestra la vista de registrar Ciudadano---
     public void goCreateCiudadano(View view)
     {
         Intent intent = new Intent(this, CreateCiudadanoActivity.class);//de donde this a donde quiero ir CreateCiudadanoActivity.class
         startActivity(intent);
+    }
+
+    //funcion que insertara campos a la base de datos mediante URL
+
+    public  String enviarDatosGET(String usu, String pas)
+    {
+        URL url=null;
+        String linea;
+        int respuesta;
+        StringBuilder resul=null;
+
+        try
+        {
+            url =new URL("http://192.168.15.18:80/AppSolidos/validacionAcceso.php?usuario="+usu+"&contrasenia="+pas);
+            HttpURLConnection conexion=(HttpURLConnection)url.openConnection();//abre la conexion de la url
+            respuesta=conexion.getResponseCode();//muestra la respuesta de la conexion
+
+            if (respuesta==HttpURLConnection.HTTP_OK)//verificamos la conexion a la data base mediante la url
+            {
+                InputStream in =new BufferedInputStream(conexion.getInputStream());//obtenemos la conexion
+                BufferedReader reader=new BufferedReader(new InputStreamReader(in));//leemos los datos que se obtuvieron de la url
+
+                while ((linea=reader.readLine())!=null)
+                {
+                    resul.append(linea);
+                }
+            }
+
+        }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resul.toString();
     }
 }
--- app/src/main/java/com/example/carlin/munisolidos/view/ReporteSolidosActivity.java
+++ app/src/main/java/com/example/carlin/munisolidos/view/ReporteSolidosActivity.java
@@ -2,6 +2,7 @@ package com.example.carlin.munisolidos.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.carlin.munisolidos.R;
 
@@ -11,5 +12,15 @@ public class ReporteSolidosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte_solidos);
+        showToolbar(getResources().getString(R.string.toolbar_titulo_reporte),true);//el true activa la flecha de atras
     }
+
+    public void showToolbar(String titulo, boolean upButton)
+    {
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(titulo);//asigna el titulo al toobar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton); //asigna la flecha de regreso al toolbar
+    }
+
 }
